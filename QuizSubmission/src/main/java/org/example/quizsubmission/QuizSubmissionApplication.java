f813cf1dd00ecbe5d71c62f95d952a055de718be
Jafar Farwaneh <jafarfarwana@gmail.com>
2024-03-02 11:30:34 +0300

    test CI/CD pipeline

--- QuizSubmission/src/main/java/org/example/quizsubmission/QuizSubmissionApplication.java
+++ QuizSubmission/src/main/java/org/example/quizsubmission/QuizSubmissionApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizSubmissionApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(QuizSubmissionApplication.class, args);
 	}
